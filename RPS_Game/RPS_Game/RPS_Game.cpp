5fcf6d24418dde54ac65255d6f336a1eef1ee6ff
glydokid <glydokidd@naver.com>
2022-03-18 02:36:59 +0900

    Update RPS_Game.cpp

--- RPS_Game/RPS_Game/RPS_Game.cpp
+++ RPS_Game/RPS_Game/RPS_Game.cpp
@@ -3,35 +3,39 @@
 #include <time.h> //time 함수
 
 //사람 VS 컴퓨터 비교 함수
-//게임머니 계산
-int comVShu(int hu, int com, int baeting, int hu_money, int com_money) {
+int _comVShu(int hu, int com) {
 
 	if (hu == com) {
 		printf("비겼습니다.\n");
-		printf("현재 남은 게임머니는 %d원 입니다.\n", hu_money);
+		return 0;
 	}
 
 	else if (com > (hu + 1) % 3) {
 		printf("이겼습니다.\n");
-		hu_money += baeting;
-		com_money -= baeting;
-		printf("현재 남은 게임머니는 %d원 입니다.\n", hu_money);
-
-		if (com_money <= 0)
-			printf("@@@플레이어가 승리했습니다.@@@\n");
+		return 1;
 	}
 
 	else {
 		printf("졌습니다.\n");
-		hu_money -= baeting;
-		com_money += baeting;
-		printf("현재 남은 게임머니는 %d원 입니다.\n", hu_money);
-
-		if (hu_money <= 0)
-			printf("@@@플레이어가 패배하였습니다.@@@\n");
+		return -1;
 	}
+	
+}
 
-	return hu_money, com_money;
+//게임머니 계산
+int _result_money(int hu_money, int com_money,int baeting, int vs_result) {
+	if (vs_result == 0)
+		printf("사용자의 남은 게임머니는 %d원 입니다.\n", hu_money);
+
+	else if (vs_result > 0) {
+		hu_money += baeting;
+		printf("사용자의 남은 게임머니는 %d원 입니다.\n", hu_money);
+	}
+	else if (vs_result < 0) {
+		hu_money -= baeting;
+		printf("사용자의 남은 게임머니는 %d원 입니다.\n", hu_money);
+	}
+	return hu_money;
 }
 
 int main() {
@@ -42,8 +46,9 @@ int main() {
 	int Hu_game_money = Game_money;
 	int Com_game_money = Game_money;
 
-	while (Hu_game_money || Com_game_money >= 0) {
-		
+	while (true) {
+
+		//배팅금액 설정
 		int Baeting_money;
 		printf("---------------------------------------\n");
 		printf("금액을 배팅하세요:");
@@ -53,16 +58,27 @@ int main() {
 
 		//컴퓨터의 가위바위보 랜덤 출력
 		srand(time(NULL));
-		int com_RPS = 0;
+		int com_RPS;
 		com_RPS = rand() % 3 + 1;
 
 		//사람 가위바위보 입력
-		int hu_RPS = 0;
+		int hu_RPS;
 		scanf("%d", &hu_RPS);
 
 		//함수를 이용한 대결, 게임머니 계산
-		int result;
-		result = comVShu(hu_RPS, com_RPS, Baeting_money, Hu_game_money, Com_game_money);
+		int Vs_result;
+		Vs_result = _comVShu(hu_RPS, com_RPS);
+
+		//게임머니 계산
+		int Money_result;
+		Money_result = _result_money(Hu_game_money, Com_game_money, Baeting_money, Vs_result);
+		Hu_game_money = Money_result;
+		Com_game_money = (Game_money * 2) - Hu_game_money;
+
+		if (Hu_game_money <= 0 || Com_game_money <= 0) {
+			break;
+		}
 	}
+
 	return 0;
 }
