a388263a740dcf10c87a71764d07c43dab75b602
glydokid <glydokidd@naver.com>
2022-03-30 16:30:07 +0900

    22.03.30

--- Pokemom/Pokemom/PKM_main.cpp
+++ Pokemom/Pokemom/PKM_main.cpp
@@ -19,68 +19,70 @@ int main() {
 
 			printf("\n현재는 %d단계입니다.\n", i);
 
-			while (1) {
-				printf("\n------현재 스택: %d ------\n", stack);
-				if(T==1){
-					pokemon_AT = _fire_monster_attack();
-					printf("\n");
-					enemy_AT = _enemy_monster1_attack();
-					pokemon_HP = _fire_monster_HP();
-					enemy_HP = _enemy_monster1();
+			if (i == 1) {
+				while (1) {
+					printf("\n------현재 스택: %d ------\n", stack);
+					if (T == 1) {
+						pokemon_AT = _fire_monster_attack();
+						printf("\n");
+						enemy_AT = _enemy_monster1_attack();
+						pokemon_HP = _fire_monster_HP();
+						enemy_HP = _enemy_monster1();
 
-					if (pokemon_AT == 170)
-						stack = -1;
+						if (pokemon_AT == 170)
+							stack = -1;
 
-					enemy_HP -= pokemon_AT;
-					printf("적 포켓몬의 체력은: %d 남았습니다\n", enemy_HP);
-					if (enemy_HP <= 0)
-						break;
+						enemy_HP -= pokemon_AT;
+						printf("적 포켓몬의 체력은: %d 남았습니다\n", enemy_HP);
+						if (enemy_HP <= 0)
+							break;
 
-					pokemon_HP -= enemy_AT;
-					printf("포켓몬의 체력은: %d 남았습니다\n", pokemon_HP);
-					if (pokemon_HP <= 0)
-						break;
+						pokemon_HP -= enemy_AT;
+						printf("포켓몬의 체력은: %d 남았습니다\n", pokemon_HP);
+						if (pokemon_HP <= 0)
+							break;
+					}
+
+					if (T % 2 != 0 && T != 1) {
+						pokemon_AT = _fire_monster_attack();
+						printf("\n");
+						enemy_AT = _enemy_monster1_attack();
+
+						if (pokemon_AT == 170)
+							stack = -1;
+
+						enemy_HP -= pokemon_AT;
+						printf("적 포켓몬의 체력은: %d 남았습니다\n", enemy_HP);
+						if (enemy_HP <= 0)
+							break;
+
+						pokemon_HP -= enemy_AT;
+						printf("포켓몬의 체력은: %d 남았습니다\n", pokemon_HP);
+						if (pokemon_HP <= 0)
+							break;
+					}
+					else if (T % 2 == 0) {
+						enemy_AT = _enemy_monster1_attack();
+						printf("\n");
+						pokemon_AT = _fire_monster_attack();
+						printf("\n");
+
+						if (pokemon_AT == 170)
+							stack = -1;
+
+						pokemon_HP -= enemy_AT;
+						printf("포켓몬의 체력은: %d 남았습니다\n", pokemon_HP);
+						if (pokemon_HP <= 0)
+							break;
+
+						enemy_HP -= pokemon_AT;
+						printf("적 포켓몬의 체력은: %d 남았습니다\n", enemy_HP);
+						if (enemy_HP <= 0)
+							break;
+					}
+					T++;
+					stack++;
 				}
-
-				if (T % 2 != 0 && T != 1) {
-					pokemon_AT = _fire_monster_attack();
-					printf("\n");
-					enemy_AT = _enemy_monster1_attack();
-
-					if (pokemon_AT == 170)
-						stack = -1;
-					
-					enemy_HP -= pokemon_AT;
-					printf("적 포켓몬의 체력은: %d 남았습니다\n", enemy_HP);
-					if (enemy_HP <= 0)
-						break;
-
-					pokemon_HP -= enemy_AT;
-					printf("포켓몬의 체력은: %d 남았습니다\n", pokemon_HP);
-					if (pokemon_HP <= 0)
-						break;
-				}
-				else if (T % 2 == 0) {
-					enemy_AT = _enemy_monster1_attack();
-					printf("\n");
-					pokemon_AT = _fire_monster_attack();
-					printf("\n");
-
-					if (pokemon_AT == 170)
-						stack = -1;
-
-					pokemon_HP -= enemy_AT;
-					printf("포켓몬의 체력은: %d 남았습니다\n", pokemon_HP);
-					if (pokemon_HP <= 0)
-						break;
-
-					enemy_HP -= pokemon_AT;
-					printf("적 포켓몬의 체력은: %d 남았습니다\n", enemy_HP);
-					if (enemy_HP <= 0)
-						break;
-				}
-				T++;
-				stack++;
 			}
 			printf("\n");
 			printf("%d 단계를 이겼습니다.\n", i);
